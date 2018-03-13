230356b04505b24050cb64d5c25e7fb6bfaaef80
elen7c9 <enasiyati@gmail.com>
2018-03-13 15:42:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeroes;
+  
+  while( number >= 1 ){
+    zeroes += Math.floor(number / 5);
+    number = number/5;
+  }
+  
+  return(zeroes);
+
 }
